2e34e5cf248af18b189a722c416ad53bd9876615
Alex Ferreira <alexjferreira94@yahoo.com.br>
2024-03-17 16:30:49 -0300

    adding bucket config

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "techchallenge-rds-terraform	"
+    bucket = "techchallenge-rds-terraform"
     key = "terraform/terraform.tfstate"
     region = "us-east-1"
   }
