c385f050a9b6ae72d11c9ae4c57dc8b91bcd5c2e
Alex Ferreira <alexjferreira94@yahoo.com.br>
2024-03-17 18:00:40 -0300

    adding public access

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -114,6 +114,7 @@ resource "aws_db_instance" "db_instance" {
   db_subnet_group_name    = aws_db_subnet_group.database_subnet_group.name
   vpc_security_group_ids  = [aws_security_group.database_security_group.id]
   availability_zone       = "us-east-1a"
+  publicly_accessible     = true
   db_name                 = "challengeDatabaseRds"
   skip_final_snapshot     = true
 }
